e8b992dd447396641a96e10575fd00fda43d1a3b
Adib Afsar Khan <Adib Afsar Khan@DESKTOP-JC76IDV>
2026-01-21 22:41:28 +0600

    minor changes

--- UI/SeeInventory.cs
+++ UI/SeeInventory.cs
@@ -41,7 +41,7 @@ namespace UI
                 }
                 else
                 {
-                    MessageBox.Show("Product not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Product not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                 }
             }
             else
